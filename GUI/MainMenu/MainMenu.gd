04a6faa7efc91a3511fe947ac8d9780655173060
eanderson <717eanderson@gmail.com>
2020-11-24 19:09:24 -0500

    Added functionality to settings and exit button on the main menu

--- GUI/MainMenu/MainMenu.gd
+++ GUI/MainMenu/MainMenu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+const settings = preload("res://GUI/Settings/Settings.tscn")
+
+onready var settings_back = get_node("SettingsBackground")
+
 var key = "MainMenu"
 
 func _on_NewGame_pressed():
@@ -7,3 +11,15 @@ func _on_NewGame_pressed():
 
 func _on_LoadGame_pressed():
 	Global.switch_scene("World1", true)
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+func _on_Settings_pressed():
+	settings_back.visible = true
+	var settings_menu = settings.instance()
+	settings_menu.connect("settings_closed", self, "settings_close")
+	get_tree().get_root().add_child(settings_menu)
+
+func settings_close():
+	settings_back.visible = false
