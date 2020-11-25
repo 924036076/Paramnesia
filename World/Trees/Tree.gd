92aaeacd40c48770909993e29d38855b5f84aa54
eanderson <717eanderson@gmail.com>
2020-11-24 20:01:50 -0500

    Reworked oak and pine trees to use new tree class

--- World/Trees/Tree.gd
+++ World/Trees/Tree.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+class_name NatureTree
+
+const FellEffect = preload("res://World/Trees/DestroyTree.tscn")
 const MOD_AMOUNT = 0.6
 
-var trans_counter = 0
 onready var health_bar = get_node("ResourceIndicator")
 onready var sprite = get_node("Sprite")
+
+var trans_counter = 0
 
 enum {
 	RIGHT,
@@ -14,13 +18,10 @@ enum {
 func _ready():
 	health_bar.connect("no_health", self, "fell_tree")
 
-func _on_Hurtbox_area_entered(_area):
-	if PlayerData.get_item_held() == "stone_axe":
-		health_bar.health = health_bar.health - 20
-
 func fell_tree():
-	var FellEffect = load("res://World/Trees/Oak/DestroyOakTree.tscn")
 	var fellEffect = FellEffect.instance()
+	fellEffect.get_node("Sprite").position = get_node("Sprite").position
+	fellEffect.get_node("Sprite").texture = get_node("Sprite").texture
 	get_parent().add_child(fellEffect)
 	fellEffect.global_position = global_position
 	if fellEffect.global_position.x < get_tree().get_current_scene().get_node("GlobalYSort/Player").global_position.x:
@@ -29,11 +30,15 @@ func fell_tree():
 		fellEffect.fall_dir = RIGHT
 	queue_free()
 
-func _on_TransTest_area_entered(_area):
+func _on_Hurtbox_area_entered(area):
+	if PlayerData.get_item_held() == "stone_axe":
+		health_bar.health = health_bar.health - 20
+
+func _on_TransTest_area_entered(area):
 	sprite.modulate.a = MOD_AMOUNT
 	trans_counter += 1
 
-func _on_TransTest_area_exited(_area):
+func _on_TransTest_area_exited(area):
 	trans_counter -= 1
 	if trans_counter == 0:
 		sprite.modulate.a = 1
