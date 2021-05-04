7f88ab3771326cbd77be8b97a725dcffa4ec2572
eanderson <717eanderson@gmail.com>
2021-05-03 21:34:56 -0400

    character customization functionality

--- GUI/Embark/CharacterCustomize.gd
+++ GUI/Embark/CharacterCustomize.gd
@@ -2,6 +2,28 @@ extends Control
 
 var key = "Menu"
 
+var hair: int = 1
+var max_hair: int = 1
+var outfit: int = 1
+var max_outfit: int = 1
+
+onready var body_sprite = get_node("BaseSprite")
+onready var hair_sprite = get_node("HairSprite")
+onready var outfit_sprite = get_node("OutfitSprite")
+onready var animation_player = get_node("AnimationPlayer")
+onready var skin_palette = get_node("SkinPalette")
+onready var hair_palette = get_node("HairPalette")
+
+func _input(_event):
+	if Input.is_action_just_pressed("inventory_main"):
+		var cursor_pos: Vector2 = get_global_mouse_position()
+		if skin_palette.get_global_rect().has_point(cursor_pos):
+			var color: Color = skin_palette.get_node("Color" + str(int((cursor_pos.x - skin_palette.rect_global_position.x) / 20))).modulate
+			body_sprite.modulate = color
+		if hair_palette.get_global_rect().has_point(cursor_pos):
+			var color: Color = hair_palette.get_node("Color" + str(int((cursor_pos.x - hair_palette.rect_global_position.x) / 20))).modulate
+			hair_sprite.modulate = color
+
 func back():
 	Global.switch_scene("WorldSettings", false)
 
@@ -10,3 +32,37 @@ func _on_BackButton_pressed():
 
 func _on_NextButton_pressed():
 	Global.switch_scene("Supplies", false)
+
+func _on_HairMinus_pressed():
+	hair -= 1
+	if hair < 0:
+		hair = max_hair
+	update_sprites()
+
+func _on_HairPlus_pressed():
+	hair += 1
+	if hair > max_hair:
+		hair = 0
+	update_sprites()
+
+func _on_OutfitMinus_pressed():
+	outfit -= 1
+	if outfit < 0:
+		outfit = max_outfit
+	update_sprites()
+
+func _on_OutfitPlus_pressed():
+	outfit += 1
+	if outfit > max_outfit:
+		outfit = 0
+	update_sprites()
+	
+func update_sprites():
+	hair_sprite.texture = load("res://Player/Parts/hair" + str(hair) + ".png")
+	outfit_sprite.texture = load("res://Player/Parts/outfit" + str(outfit) + ".png")
+
+func _on_WalkButton_toggled(button_pressed):
+	if button_pressed:
+		animation_player.play("Walk")
+	else:
+		animation_player.stop()
